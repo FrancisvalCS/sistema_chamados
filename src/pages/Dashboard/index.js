c8eb023a25a9d5563227d93b612050551d838ba3
francisval <francisvalcs@gmail.com>
2023-05-18 17:16:01 -0300

    Criando Novo Chamado

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,10 @@
 import { useContext } from 'react'
 import {AuthContext} from '../../contexts/auth'
-
 import Header from '../../components/Header'
+import Title from '../../components/Title'
+import { FiPlus, FiMessageSquare, FiSearch,FiEdit2 } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
+import './dashboard.css'
 
 export default function Dashboard(){
   const { logout } = useContext(AuthContext);
@@ -13,6 +16,47 @@ export default function Dashboard(){
   return(
     <div>
       <Header/>
+      <div className='content'>
+        <Title>
+          <FiMessageSquare size={25}/>
+        </Title>
+
+        <>
+        <Link to="/new" className="new">
+          <FiPlus color="#FFF" size={25} />
+          Novo chamado
+        </Link>
+        <table>
+          <thead>
+            <th scope='col'>Cliente</th>
+            <th scope='col'>Assunto</th>
+            <th scope='col'>Status</th>
+            <th scope='col'>Cadastrando em</th>
+            <th scope='col'>#</th>
+          </thead>
+          <tbody>
+            <tr>
+              <td data-label="Cliente">Mercado Esquina</td>
+              <td data-label="Assunto">Suporte</td>
+              <td data-label="Status">
+                <span className='badge' style={{backgroundColor: '#999'}}>
+                  Em aberto
+                </span>  
+              </td>
+              <td data-label="Cadastrado">12/05/2022</td>
+              <td data-label="#">
+                <button className='action' style={{backgroundColor: '#3583f6'}}>
+                  <FiSearch color='#FFF' size={17}/>
+                </button>
+                <button className='action' style={{backgroundColor: '#f6a935'}}>
+                  <FiEdit2 color='#FFF' size={17}/>
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        </>
+      </div>
 
     </div>
   )
